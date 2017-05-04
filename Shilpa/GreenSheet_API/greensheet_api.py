32e7fbe27a4af5bde1fb83cc043a41498de53492
shilpa <shilpa266@gmail.com>
2017-05-04 11:49:51 -0700

    Checking file existence in mongoDB and Uploading file to mongoDB

--- Shilpa/GreenSheet_API/greensheet_api.py
+++ Shilpa/GreenSheet_API/greensheet_api.py
@@ -1,17 +1,17 @@
 from pydocx import PyDocX
-from flask import Flask
+from flask import Flask, render_template, request
 from flask import jsonify
 import json
 from bson import json_util
 from pymongo import MongoClient
 import sys
 import re
+from werkzeug.utils import secure_filename
 from BeautifulSoup import BeautifulSoup
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-html = PyDocX.to_html(open('cmpe273-greensheet.docx', 'rb'))
 
 app = Flask(__name__);
 client = MongoClient('mongodb://localhost:27017/')
@@ -22,13 +22,28 @@ def hello_world():
 
    return "Hello World"
 
-@app.route('/cmpe273')
-def find_document():
-    db.greennsheetdocs.insert({
-        "filename":"CMPE273"
-
+@app.route('/find_doc/<collection_name>')
+def find_document(collection_name):
+    if(collection_name in db.collection_names()):
+      return "Success"
+    else:
+      return "File Not Found"
+@app.route('/upload')
+def upload_file():
+   return render_template('upload.html')
+    
+@app.route('/uploader',methods = ['GET', 'POST'])
+def insert_document():   
+   if request.method == 'POST':
+      f = request.files['file']
+   f.save(secure_filename(f.filename))
+   html = PyDocX.to_html(open(f.filename, 'rb'))
+   db.greensheetdocs.insert({
+        "filename":"CMPE273",
+        "HTML":html
     })
-    return "Success"
+   return "Successfully Uploaded"
+
 @app.route("/get/<value>")
 def get_content(value):
     collection=db.greensheetdocs.find()
